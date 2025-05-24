11e1e102de1d2b7794698e9c162918add4941526
umangjit Singh <umangjitsingh@gmail.com>
2025-05-24 16:40:46 -0400

    custom validation mongoose

--- src/app.js
+++ src/app.js
@@ -10,26 +10,111 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Post user
 app.post('/signup', async (req, res) => {
 	try {
 		const {
-			firstName, lastName, emailId, password, age, gender
+			firstName, lastName, emailId, password, age, gender, about, skills, photoUrl
 		} = req.body;
 		const user = new User({
-			firstName, lastName, emailId, password, age, gender
+			firstName, lastName, emailId, password, age, gender, about, skills, photoUrl
 		})
-		console.log(user)
 
 		await user.save();
-		res.status(201).json({
-			message: 'user created',
-			success: true,
-			user
+		return res.status(201).json({
+			message: 'user created', success: true, user
+		})
+	} catch (e) {
+		return res.status(400).json({
+			message: e.message, success: false
+		})
+	}
+});
+
+// Feed API - Get/feed - get all users from db.
+app.get('/feed', async (req, res) => {
+	try {
+		const users = await User.find({});
+		if (!users) {
+			return res.status(404).json({
+				message: "no users found", success: false
+			})
+		}
+		return res.status(200).json({
+			message: "list of all users", users
+		})
+	} catch (e) {
+		return res.status(400).json({
+			message: `Something went wrong :${e}`, success: false
+		})
+	}
+
+});
+
+// Get user by email
+app.get('/user', async (req, res) => {
+	try {
+		const {emailId} = req.body;
+		const user = await User.findOne({emailId});
+		if (!user) {
+			res.status(404).json({
+				message: "User not found", success: false
+			})
+		}
+		res.status(200).json({
+			success: true, user
 		})
 	} catch (e) {
 		res.status(400).json({
-			message: e.message,
-			success: false
+			message: `Something went wrong :${e}`, success: false
+		})
+	}
+
+})
+
+// Delete a user
+app.delete('/user/:id', async (req, res) => {
+	try {
+		const userId = req.params.id;
+
+		const user = await User.findByIdAndDelete({_id: userId.toString()})
+		if (!user) {
+			return res.status(404).json({
+				message: "no user found", success: false
+			})
+		}
+		return res.status(200).json({
+			message: `${user.firstName} is deleted`, success: true,
+		})
+	} catch (e) {
+		return res.status(400).json({
+			message: e.message, success: false
+		})
+	}
+})
+
+// update a user
+app.patch('/user/:id', async (req, res) => {
+	try {
+		const userId = req.params.id;
+		const {
+			firstName, lastName, password, age, gender, about, skills, photoUrl
+		} = req.body;
+		const user = await User.findByIdAndUpdate({_id: userId}, {
+			firstName, lastName, age, gender, password, about, skills, photoUrl
+		}, {returnDocument: 'after'});
+		if (!user) {
+			return res.status(400).json({
+				message: "user cannot be updated", success: false
+			})
+		}
+		return res.status(201).json({
+			message: 'user updated', success: true, user
+		})
+
+	} catch (e) {
+		return res.status(400).json({
+			message: e.message, success: false
 		})
 	}
 })
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,25 +3,60 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
 	firstName: {
-		type    : String,
-		required: true,
+		type     : String,
+		required : true,
+		lowercase: true,
+		trim     : true,
+		minLength: 3,
+		maxLength: 60
 	},
 	lastName : {
-		type: String,
+		type     : String,
+		lowercase: true,
+		trim     : true
 	},
 	emailId  : {
-		type  : String,
-		unique: true,
+		type     : String,
+		required : true,
+		unique   : true,
+		lowercase: true,
+		trim     : true
+
 	},
 	password : {
 		type: String,
+		trim: true
 	},
 	age      : {
 		type: Number,
+		min : 18
 	},
 	gender   : {
 		type: String,
-		enum: ['male', 'female', 'others']
+		enum: ['male', 'female', 'others'],
+		validate(value){
+			if(!['male', 'female', 'others'].includes(value)){
+				throw new Error('Gender data is not valid')
+			}
+		}
+	},
+	photoUrl : {
+		type   : String,
+		default: "https://cdn-icons-png.flaticon.com/512/196/196369.png"
+	},
+	about    : {
+		type   : String,
+		default: "Please enter something about yourself.."
+	},
+	skills   : {
+		type: [String],
+		validate(value){
+			if(value.length > 7){
+				console.log(value)
+
+				throw new Error('Only 7 skills allowed')
+			}
+		}
 	}
 }, {timestamps: true});
 
